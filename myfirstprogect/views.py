74cab346dc74866df6baef4ab597a5fe148d3147
Руслан Гарифуллин <ruslan102gar@gmail.com>
2022-01-16 14:51:27 +0300

    Add words count

--- myfirstprogect/views.py
+++ myfirstprogect/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def about(request):
-	return HttpResponse('This is about page')
-
 def home(request):
 	return render(request, 'home.html')
+
+def reverse(request):
+	home_text = request.GET['usertext']
+
+	arr = home_text.split()
+
+	len_arr = len(arr) 
+
+
+	reverse_text = home_text[::-1]
+
+	return render(request, 'rev.html', {'usertext':home_text, "revtext":reverse_text, "count":len_arr})
+
+	
